5c4a095c1fc5bc8899cc9042b800d119348d7800
DCFdd <142072261+DCFdd@users.noreply.github.com>
2023-08-12 12:18:52 +0800

    Add files via upload

---
+++ 02：变量输出/02：变量输出/源.cpp
@@ -0,0 +1,21 @@
+#include<iostream>//库：iostream
+using namespace std;/*程序中频繁出现 cout ，这样的操作极为不便
+								       后面程序中可以直接使用 cout 
+									   而且std中的其他名字也可以直接使用，如 cin 、 endl 等*/
+int w-()
+{
+
+	int a = 10;
+	
+	std::cout << "a ="<<a << endl;//输出格式
+
+	//endl：换行操作
+
+
+	system("pause");//这个语句会在程序运行时暂停程序的执行，
+							    //并在控制台窗口中显示一条消息，提示用户按任意键继续。
+								//pause：意思是短暂的中断或停止，含再进行下去的意味。
+
+
+	return 0;
+}
